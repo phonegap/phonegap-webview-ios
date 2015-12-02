b85569d1b996ab141bdfb4a94053686cda7e4dd2
Holly Schinsky <hollyschinsky@gmail.com>
2015-12-02 18:57:53 -0500

    Added file transfer plugin

--- resources/www/cordova_plugins.js
+++ resources/www/cordova_plugins.js
@@ -475,6 +475,7 @@ module.exports.metadata =
     "cordova-plugin-camera": "1.2.0",
     "cordova-plugin-contacts": "1.1.0",
     "cordova-plugin-file": "3.0.0",
+    "cordova-plugin-file-transfer": "1.3.0",
     "cordova-plugin-media-capture": "1.0.1",
     "cordova-plugin-globalization": "1.0.1"
 }
